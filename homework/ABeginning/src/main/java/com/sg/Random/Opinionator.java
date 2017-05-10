912c1c3164f8d67ed0107442e5faccf8091cdedf
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-10 19:22:56 -0400

    KPIETSCHWORKSELF-4 #comment Complete Opinionator #time 10m

---
+++ homework/ABeginning/src/main/java/com/sg/Random/Opinionator.java
@@ -0,0 +1,31 @@
+package com.sg.Random;
+import java.util.Random;
+public class Opinionator {
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        System.out.println("Not sure what's my fav video game.");
+        System.out.println("I'll leave it to Random.");
+        
+        int x = randomizer.nextInt(5);
+        
+        System.out.println("Random number generated was: "+x);
+        
+        switch(x){
+            case 0: System.out.println("Baldur's Gate II");
+                    break;
+            case 1: System.out.println("Pokemon");
+                    break;
+            case 2: System.out.println("Harvest Moon");
+                    break;
+            case 3: System.out.println("DDR");
+                    break;
+            case 4: System.out.println("Fallout: New Vegas");
+            // The bug in the original code was the inclusion
+            // of the if statement for x == 5.
+            // Random.nextInt(5) will never generate a 5.
+        }
+        
+        System.out.println("Oh yeah!  Thanks, Random, I'd forgotten"
+                + "that was my favorite.");
+    }
+}
