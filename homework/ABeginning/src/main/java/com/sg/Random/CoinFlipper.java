22b5258d3281c8867645b01138f1502048c549f1
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-10 19:53:31 -0400

    KPIETSCHWORKSELF-4 #comment Complete CoinFlipper #time 3m

---
+++ homework/ABeginning/src/main/java/com/sg/Random/CoinFlipper.java
@@ -0,0 +1,11 @@
+package com.sg.Random;
+import java.util.Random;
+public class CoinFlipper {
+    public static void main(String[] args) {
+        Random coinFlip = new Random();
+        System.out.println("Flipping your coin...");
+        Boolean heads = coinFlip.nextBoolean();
+        if(heads){System.out.println("You got heads.");}
+        else{System.out.println("You got tails.");}
+    }
+}
