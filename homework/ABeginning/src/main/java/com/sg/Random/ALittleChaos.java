66f10bac31f67a85261d98194560c56902082b92
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-10 19:13:04 -0400

    KPIETSCHWORKSELF-4 #comment Complete ALittleChaos #time 10m

---
+++ homework/ABeginning/src/main/java/com/sg/Random/ALittleChaos.java
@@ -0,0 +1,27 @@
+package com.sg.Random;
+import java.util.Random;
+
+public class ALittleChaos {
+    public static void main(String[] args) {
+        Random randomizer = new Random();
+        
+        System.out.println("Random can make integers: "+randomizer.nextInt());
+        System.out.println("Or a double: "+randomizer.nextDouble());
+        System.out.println("Or a boolean: "+randomizer.nextBoolean());
+        
+        int num = randomizer.nextInt(100);
+        // If changed to randomizer.nextInt(50)+50, you're
+        // guaranteed a minimum value of 49.
+        
+        System.out.println("You can store a random result: "+num);
+        System.out.println("And use it over and over again: "+num+", "+num);
+        
+        System.out.println("Or just keep generating new values.");
+        System.out.println("Here're some examples: ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.print(randomizer.nextInt(101) + ", ");
+        System.out.print(randomizer.nextInt(101));
+    }
+}
