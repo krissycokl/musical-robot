142557ca9b823ea7901864e9af1787c062b09f73
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-10 22:07:38 -0400

    KPIETSCHWORKSELF-5 #comment Complete LazyTeenager #time 15m

---
+++ homework/ABeginning/src/main/java/com/sg/PP6WhilesAndDos/LazyTeenager.java
@@ -0,0 +1,27 @@
+package com.sg.PP6WhilesAndDos;
+
+import java.util.Random;
+
+public class LazyTeenager {
+    public static void main(String[] args) {
+        Random ran = new Random();
+        
+        double chance = 0.0;
+        int timesTold = 0;
+        
+        do {
+            timesTold++;
+            System.out.println("Clean your room! (x"+timesTold+")");
+            chance += .01;
+            if (ran.nextDouble()<chance){
+                System.out.println("Sorry, mother.  Of course I'll clean.");
+                System.out.println("That's a necessary part of belonging to a family.");
+                break;
+            }
+            if (timesTold == 15){
+                System.out.println("Forget it. I'm smashing your GameCube.");
+                break;
+            }
+        } while (timesTold < 15);
+    }
+}
