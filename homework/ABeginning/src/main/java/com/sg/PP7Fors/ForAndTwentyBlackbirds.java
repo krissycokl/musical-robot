7b7521dddb07b67e4d5f1cc816d2a7756f6bc1a9
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-10 22:16:42 -0400

    KPIETSCHWORKSELF-6 #comment Complete ForAndTwentyBlackbirds #time 15m

---
+++ homework/ABeginning/src/main/java/com/sg/PP7Fors/ForAndTwentyBlackbirds.java
@@ -0,0 +1,13 @@
+package com.sg.PP7Fors;
+
+public class ForAndTwentyBlackbirds {
+    public static void main(String[] args) {
+        
+        int i;
+        
+        for (i=0;i<24;i++){
+            System.out.println("Blackbird #"+(i+1)+" in the pie.");
+        }
+        System.out.println("Now there are "+i+" birds in the pie.");
+    }
+}
