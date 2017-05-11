9f158623fc2b39bf4a6ab728af9dfe38fba25ec0
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-11 09:45:10 -0400

    KPIETSCHWORKSELF-6 #comment Complete ForByFor #time 10m

---
+++ homework/ABeginning/src/main/java/com/sg/PP7Fors/ForByFor.java
@@ -0,0 +1,25 @@
+package com.sg.PP7Fors;
+
+public class ForByFor {
+    public static void main(String[] args) {
+        for (int i=0;i<3;i++){
+            System.out.print("|");
+            
+            for (int j=0;j<3;j++){
+                for (int k=0; k<3; k++){
+                    if(i==1 && j==1){
+                        System.out.print("#");
+                    }else if (i==1) {
+                        System.out.print("@");
+                    }else if (j==1){
+                        System.out.print("$");
+                    }else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.print("|");
+            }
+            System.out.println("");
+        }
+    }
+}
