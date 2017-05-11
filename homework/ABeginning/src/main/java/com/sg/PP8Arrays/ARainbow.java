b1a0a6379ff8d05f198846550f17872699e9bf29
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-11 16:25:36 -0400

    KPIETSCHWORKSELF-8 #comment Complete ARainbow #time 10m

---
+++ homework/ABeginning/src/main/java/com/sg/PP8Arrays/ARainbow.java
@@ -0,0 +1,23 @@
+package com.sg.PP8Arrays;
+
+public class ARainbow {
+    public static void main(String[] args) {
+        String[] colors = {
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Indigo",
+            "VIOLENT"
+        };
+                
+        System.out.println(colors[0]);
+        System.out.println(colors[1]);
+        System.out.println(colors[2]);
+        System.out.println(colors[3]);
+        System.out.println(colors[4]);
+        System.out.println(colors[5]);
+        System.out.println(colors[6]);
+    }
+}
