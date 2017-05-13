c81e2083b31f02d3cbbccb7d0f9ad1ab016a1524
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-12 20:25:47 -0400

    KPIETSCHWORKSELF-9 #comment Update HealthyHearts to use Math.round #time 10m

--- homework/ABeginning/src/main/java/com/sg/scanner/HealthyHearts.java
+++ homework/ABeginning/src/main/java/com/sg/scanner/HealthyHearts.java
@@ -8,17 +8,10 @@ public class HealthyHearts {
         System.out.println("How old are you, ya geezer?");
         age = Integer.parseInt(myScanner.nextLine());
         
-        System.out.println("Your heart shouldn't work above " + (220-age)
+        System.out.println("Your shouldn't exert yourself past " + (220-age)
                 + " bpm.");
      
-        //System.out.println("Your target range is " + (.5*(220-age))
-        //        + "-" + (.85*(220-age)) + " bpm.");
-        
-        //System.out.println("Your target range is " + ((220-age)/2)
-        //        + "-" + ((220-age)*17/20) + " bpm.");
-        
-        System.out.println("Your target range is " + 
-                ((int) (.5*(220-age))) + "-"
-                + ((int) (.85*(220-age))) + " bpm.");
+        System.out.println("Your target range is " + Math.round(.5*(220-age))
+                + "-" + Math.round(.85*(220-age)) + " bpm.");
     }
 }
