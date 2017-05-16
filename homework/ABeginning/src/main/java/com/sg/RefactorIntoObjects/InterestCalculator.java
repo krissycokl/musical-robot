e8bd4832a8640ea64c44b139ef8cb845cc7cad06
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-16 12:24:12 -0400

    KPIETSCHWORKSELF-11 #comment Add classes to model accounts and years, as well as the UI #time 2h

--- homework/ABeginning/src/main/java/com/sg/RefactorIntoObjects/InterestCalculator.java
+++ homework/ABeginning/src/main/java/com/sg/RefactorIntoObjects/InterestCalculator.java
@@ -1,5 +1,31 @@
 package com.sg.RefactorIntoObjects;
 
 public class InterestCalculator {
-
+    public static void main(String[] args) {
+        
+        double balBegin;
+        double i;
+        int compoundings;
+        int numYears;    
+        
+        balBegin = InterestCalcUI.getBeginningBalance();
+        i = InterestCalcUI.getInterestRate();
+        compoundings = InterestCalcUI.getCompoundings();
+        if (compoundings == 0){
+            System.out.println("Invalid input.");
+            System.exit(0);
+        }
+        numYears = InterestCalcUI.getYears();
+        
+        InterestCalcAccount account = new InterestCalcAccount(
+                balBegin,
+                i,
+                compoundings,
+                numYears
+        );
+        
+        for (int iter=1;iter<= numYears;iter++){
+            account.addYear(iter);
+        }   
+    }
 }
