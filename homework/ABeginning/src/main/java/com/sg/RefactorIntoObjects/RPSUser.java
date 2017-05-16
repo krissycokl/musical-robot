106524dcccbc75d92d06a4eb047e9a2fe238af0d
Kristen Pietsch <kristen.pietsch@gmail.com>
2017-05-16 14:32:34 -0400

    KPIETSCHWORKSELF-10 #comment Refactor RockPaperScissors into UI, logic, user, and app classes #time 1h 30m

---
+++ homework/ABeginning/src/main/java/com/sg/RefactorIntoObjects/RPSUser.java
@@ -0,0 +1,32 @@
+package com.sg.RefactorIntoObjects;
+
+public class RPSUser {
+    private int hand;
+    private int wins;
+    private final String NAME;
+    
+    RPSUser(String name){
+        wins = 0;
+        this.NAME = name;
+    }
+    
+    public void addWin(){
+        wins++;
+    }
+    
+    public int getWins(){
+        return wins;
+    }
+    
+    public void setHand(int hand){
+        this.hand = hand;
+    }
+    
+    public int getHand(){
+        return this.hand;
+    }
+    
+    public String getName(){
+        return this.NAME;
+    }
+}
